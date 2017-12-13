8f0ac6be8e0e1da6af22cc682df144f4e2e00caa
Stella Sajcic <ssajcic@tria.hr>
2017-12-13 16:04:04 +0100

    examples of prolog for beginners

---
+++ 2/.flora_aux_files/doktori.pl
@@ -0,0 +1,350 @@
+
+:-(compiler_options([xpp_on,canonical])).
+
+/********** Tabling and Trailer Control Variables ************/
+
+#define EQUALITYnone
+#define INHERITANCEflogic
+#define TABLINGreactive
+#define TABLINGvariant
+#define CUSTOMnone
+
+#define FLORA_INCREMENTAL_TABLING 
+
+/************************************************************************
+  file: headerinc/flrheader_inc.flh
+
+  Author(s): Guizhen Yang
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+#mode standard Prolog
+
+#include "flrheader.flh"
+#include "flora_porting.flh"
+
+/***********************************************************************/
+
+#define FLORA_COMPILATION_ID 6
+
+/************************************************************************
+  file: headerinc/flrheader2_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+  It includes files that must be included in the header and typically
+  contain some Prolog statements. Such files cannot appear
+  in flrheader.flh because flrheader.flh is included in various restricted
+  contexts where Prolog statements are not allowed.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#define TABLING_CONNECTIVE  :-
+
+%% flora_tabling_methods is included here to affect preprocessing of
+%% flrtable/flrhilogtable.flh dynamically
+#include "flora_tabling_methods.flh"
+
+#ifndef FLORA_NONTABLED_MODULE
+#include "flrtable.flh"
+#include "flrhilogtable.flh"
+#endif
+
+#include "flrtable_always.flh"
+
+#include "flrauxtables.flh"
+
+%% include list of tabled predicates
+#if defined(FLORA_FLT_FILENAME)
+#include FLORA_FLT_FILENAME
+#endif
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrdyna_inc.flh
+
+  Author(s): Chang Zhao
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#define TABLING_CONNECTIVE  :-
+
+#include "flrdyndeclare.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrindex_P_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#include "flrindex_P.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrdefinition_inc.flh
+
+  Author(s): Guizhen Yang
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+#include "flrdefinition.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrtrailerregistry_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+#include "flrtrailerregistry.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrrefreshtable_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#include "flrrefreshtable.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrdynamic_connectors_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#include "flrdynamic_connectors.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: syslibinc/flrimportedcalls_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the FLORA-2 compiler.
+************************************************************************/
+
+%% Loads the file with all the import statements for predicates
+%% that must be known everywhere
+
+:-(compiler_options([xpp_on])).
+
+#mode standard Prolog
+
+#if !defined(FLORA_TERMS_FLH)
+#define FLORA_TERMS_FLH
+#include "flora_terms.flh"
+#endif
+
+?-(:(flrlibman,flora_load_library(FLLIBIMPORTEDCALLS))).
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrpatch_inc.flh
+
+  Author(s): Guizhen Yang
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+#include "flrexportcheck.flh"
+#include "flrpatch.flh"
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flropposes_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+#include "flropposes.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrhead_dispatch_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#include "flrhead_dispatch.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: syslibinc/flrclause_inc.flh
+
+  Author(s): Chang Zhao
+
+  This file is automatically included by the FLORA-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#mode standard Prolog
+
+#if !defined(FLORA_TERMS_FLH)
+#define FLORA_TERMS_FLH
+#include "flora_terms.flh"
+#endif
+
+?-(:(flrlibman,flora_load_library(FLLIBCLAUSE))).
+
+/***********************************************************************/
+
+ 
+#if !defined(FLORA_FDB_FILENAME)
+#if !defined(FLORA_LOADDYN_DATA)
+#define FLORA_LOADDYN_DATA
+#endif
+#define FLORA_FDB_FILENAME  'doktori.fdb'
+?-(:(flrutils,flora_loaddyn_data(FLORA_FDB_FILENAME,FLORA_THIS_MODULE_NAME,'fdb'))).
+#else
+#if !defined(FLORA_READ_CANONICAL_AND_INSERT)
+#define FLORA_READ_CANONICAL_AND_INSERT
+#endif
+?-(:(flrutils,flora_read_canonical_and_insert(FLORA_FDB_FILENAME,FLORA_THIS_FDB_STORAGE))).
+#endif
+
+ 
+#if !defined(FLORA_FLM_FILENAME)
+#if !defined(FLORA_LOADDYN_DATA)
+#define FLORA_LOADDYN_DATA
+#endif
+#define FLORA_FLM_FILENAME  'doktori.flm'
+?-(:(flrutils,flora_loaddyn_data(FLORA_FLM_FILENAME,FLORA_THIS_MODULE_NAME,'flm'))).
+#else
+#if !defined(FLORA_READ_CANONICAL_AND_INSERT)
+#define FLORA_READ_CANONICAL_AND_INSERT
+#endif
+?-(:(flrutils,flora_read_descriptor_metafacts_canonical_and_insert(doktori,_ErrNum))).
+#endif
+
+ 
+#if !defined(FLORA_FLD_FILENAME)
+#if !defined(FLORA_LOADDYN_DATA)
+#define FLORA_LOADDYN_DATA
+#endif
+#define FLORA_FLD_FILENAME  'doktori.fld'
+?-(:(flrutils,flora_loaddyn_data(FLORA_FLD_FILENAME,FLORA_THIS_MODULE_NAME,'fld'))).
+#else
+#if !defined(FLORA_READ_CANONICAL_AND_INSERT)
+#define FLORA_READ_CANONICAL_AND_INSERT
+#endif
+?-(:(flrutils,flora_read_canonical_and_insert(FLORA_FLD_FILENAME,FLORA_THIS_FLD_STORAGE))).
+#endif
+
+ 
+#if !defined(FLORA_FLS_FILENAME)
+#if !defined(FLORA_LOADDYN_DATA)
+#define FLORA_LOADDYN_DATA
+#endif
+#define FLORA_FLS_FILENAME  'doktori.fls'
+?-(:(flrutils,flora_loaddyn_data(FLORA_FLS_FILENAME,FLORA_THIS_MODULE_NAME,'fls'))).
+#else
+#if !defined(FLORA_READ_CANONICAL_AND_INSERT)
+#define FLORA_READ_CANONICAL_AND_INSERT
+#endif
+?-(:(flrutils,flora_read_symbols_canonical_and_insert(FLORA_FLS_FILENAME,FLORA_THIS_FLS_STORAGE,_SymbolErrNum))).
+#endif
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Rule signatures %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+?-(fllibinsrulesig(4,'doktori.flr','_$_$_flora''descr_vars',FLORA_THIS_MODULE_NAME,4,FLORA_THIS_WORKSPACE(d^mvd)(__osoba,flapply(starost,__sada),__starost,'_$ctxt'(_CallerModuleVar,4,__newcontextvar1)),','(FLORA_THIS_WORKSPACE(d^mvd)(__osoba,godiste,__godiste,'_$ctxt'(FLORA_THIS_MODULE_NAME,__newcontextvar2,4)),fllibdelayedliteral('\\is','doktori.flr',56,[__starost,-(__sada,__godiste)])),null,'_$_$_flora''rule_enabled'(4,'doktori.flr',FLORA_THIS_MODULE_NAME),fllibexecute_delayed_calls([__godiste,__osoba,__sada,__starost],[__osoba,__sada,__starost]),true)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Rules %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+:-(FLORA_THIS_WORKSPACE(static^mvd)(__osoba,flapply(starost,__sada),__starost,'_$ctxt'(_CallerModuleVar,4,__newcontextvar1)),','('_$_$_flora''rule_enabled'(4,'doktori.flr',FLORA_THIS_MODULE_NAME),','(','(FLORA_THIS_WORKSPACE(d^mvd)(__osoba,godiste,__godiste,'_$ctxt'(FLORA_THIS_MODULE_NAME,__newcontextvar2,4)),fllibdelayedliteral('\\is','doktori.flr',56,[__starost,-(__sada,__godiste)])),fllibexecute_delayed_calls([__godiste,__osoba,__sada,__starost],[__osoba,__sada,__starost])))).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Signatures for latent queries %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% Queries found in the source file %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+ 
+#if !defined(FLORA_FLS2_FILENAME)
+#if !defined(FLORA_LOADDYN_DATA)
+#define FLORA_LOADDYN_DATA
+#endif
+#define FLORA_FLS2_FILENAME  'doktori.fls2'
+?-(:(flrutils,flora_loaddyn_data(FLORA_FLS2_FILENAME,FLORA_THIS_MODULE_NAME,'fls2'))).
+#else
+#if !defined(FLORA_READ_CANONICAL_AND_INSERT)
+#define FLORA_READ_CANONICAL_AND_INSERT
+#endif
+?-(:(flrutils,flora_read_symbols_canonical_and_insert(FLORA_FLS2_FILENAME,FLORA_THIS_FLS_STORAGE,_SymbolErrNum))).
+#endif
+
+/************************************************************************
+  file: headerinc/flrtrailer_inc.flh
+
+  Author(s): Michael Kifer
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+#include "flrtrailer.flh"
+
+/***********************************************************************/
+
+/************************************************************************
+  file: headerinc/flrpreddef_inc.flh
+
+  Author(s): Chang Zhao
+
+  This file is automatically included by the Flora-2 compiler.
+************************************************************************/
+
+:-(compiler_options([xpp_on])).
+
+#include "flrpreddef.flh"
+
+/***********************************************************************/
+
